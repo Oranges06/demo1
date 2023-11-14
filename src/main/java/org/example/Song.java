5247fd796863aa28fd60e18d854db415dfed9c52
Oranges06 <58148430@qq.com>
2023-11-15 00:43:25 +0800

    second

---
+++ src/main/java/org/example/Song.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Song {
+    private Integer id;
+    private String name;
+    private String singer;
+    private String cover;
+    private String path;
+    private String lyric;
+}
